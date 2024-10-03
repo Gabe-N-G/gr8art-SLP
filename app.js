76e6e38eb14f3a3647e1344d2045bbb6beae4826
g g <gabedotgutierrez@gmail.com>
2024-10-03 14:24:41 -0400

    DONE

--- app.js
+++ app.js
@@ -3,39 +3,46 @@ console.log("hello world")
 let currentColor = "white"
 let colorArray = []
 
+// const sampleGrid = ['yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'yellow', 'yellow', 'green', 'yellow', 'yellow', 'yellow', 'green', 'green', 'green', 'green', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow']
 
 const container = document.querySelector(".flex-container")
 const colorSelect = document.querySelector("#color-select")
-const cells = document.querySelectorAll(".flex-item")
+let cells;
+
 
 function makeGrid() {
     for(let i = 0; i < 64; i++){
         let div = document.createElement("div")
         div.classList.add("flex-item")
         div.setAttribute("id",`cell`+ i)
-        div.textContent = i
+        // div.style.backgroundColor = sampleGrid[i]
         container.appendChild(div)
     }
+    cells = document.querySelectorAll(".flex-item")
 }
 makeGrid()
 
 
+
 function collectColors() {
+    colorArray = []
     cells.forEach(cell => {
-        colorArray.push(cell.backgroundColor)
+        colorArray.push(cell.style.backgroundColor)
         console.log(colorArray)
     });
 }
 
 function handleClick(e) {
-    console.log(`you've clicked ${e.target.id}`)
+    // console.log(`you've clicked ${e.target.id}`)
     e.target.style.backgroundColor = currentColor
+    // console.log(e.target)
+    // console.log(e.target.style.backgroundColor)
     collectColors()
 }
 
 function handleChange(e){
     currentColor = e.target.value
-    console.log(currentColor)
+    // console.log(currentColor)
 }
 
 container.addEventListener("click",handleClick)
